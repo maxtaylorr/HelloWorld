0288117ad22a58c866c8c2b43aad504da1742ade
Max Taylor <mjttk5@mail.missouri.edu>
2018-08-27 20:40:47 -0500

    Finished project

---
+++ Hello World/HelloWorldViewController.swift
@@ -0,0 +1,47 @@
+//
+//  HelloWorldViewController.swift
+//  Hello World
+//
+//  Created by Max Taylor on 8/27/18.
+//  Copyright © 2018 Max Taylor. All rights reserved.
+//
+
+import UIKit
+
+class HelloWorldViewController: UIViewController {
+
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    @IBAction func helloButton(_ sender: Any) {
+        welcomeLabel.text = "Hello World!"
+    }
+    
+    @IBAction func clearButton(_ sender: Any) {
+        welcomeLabel.text = ""
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        welcomeLabel.text = "Welcome!"
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
